7eebc8a8a515f448689dd1d8ccb2c74384798eed
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-12-11 00:42:34 +1200

    first commit

---
+++ src/main/java/com/example/service_pets/ServicePetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/service_pets/ServicePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
